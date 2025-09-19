6480f91d320d6b76fc4ecc8b13b9589bca5851e3
Yasin Tajali <pr0grmer6cd@gmail.com>
2025-09-19 23:43:46 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,139 @@
+import { Oval } from 'react-loader-spinner';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFrown } from '@fortawesome/free-solid-svg-icons';
+import './App.css';
+
+function WeatherApp() {
+    const [input, setInput] = useState('');
+    const [weather, setWeather] = useState({
+        loading: false,
+        data: {},
+        error: false,
+    });
+
+    const toDateFunction = () => {
+        const months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ];
+        const WeekDays = [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+        ];
+        const currentDate = new Date();
+        const date = `${WeekDays[currentDate.getDay()]} ${currentDate.getDate()} ${months[currentDate.getMonth()]}`;
+        return date;
+    };
+
+    const search = async (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+
+            if (!input.trim()) return; 
+
+            // setWeather({ ...weather, loading: true, error: false });
+
+            const api_key = '10eccf27e80ed319fcc42512b3c6d1d8';
+            const url = 'https://api.openweathermap.org/data/2.5/weather';
+
+            try {
+                const res = await axios.get(url, {
+                    params: {
+                        q: `${input.trim()}`,
+                        units: 'metric',
+                        appid: api_key,
+                    },
+                });
+
+                setWeather({ data: res.data, loading: false, error: false });
+                
+            } catch (error) {
+                setWeather({ data: {}, loading: false, error: true });
+                
+                console.error('Error fetching weather:', error);
+            };
+
+            setInput('');
+        }
+    };
+
+
+    return (
+        <div className="App">
+            <h1 className="app-name">
+            let's see how's the weather of the world today !
+            </h1>
+            <div className="search-bar">
+                <input
+                    type="text"
+                    className="weather-searcher"
+                    placeholder="Enter City Name.."
+                    name="query"
+                    value={input}
+                    onChange={(event) => setInput(event.target.value)}
+                    onKeyDown={search}
+                />
+            </div>
+            {weather.loading && (
+                <>
+                    <br />
+                    <br />
+                    <Oval type="Oval" color="black" height={100} width={100} />
+                </>
+            )}
+            {weather.error && (
+                <>
+                    <br />
+                    <br />
+                    <span className="error-message">
+                        <FontAwesomeIcon icon={faFrown} />
+                        <span style={{ fontSize: '20px' }}>City not found</span>
+                    </span>
+                </>
+            )}
+            {weather && weather.data && weather.data.main && (
+                <div>
+                    <div className="city-name">
+                        <h2>
+                            {weather.data.name}, <span>{weather.data.sys.country}</span>
+                        </h2>
+                    </div>
+                    <div className="date">
+                        <span>{toDateFunction()}</span>
+                    </div>
+                    <div className="icon-temp">
+                        <img
+                            src={`https://openweathermap.org/img/wn/10d@2x.png`}
+                            alt={weather.data.weather[0].description}
+                        />
+                        {Math.round(weather.data.main.temp)}
+                        <sup className="deg">°C</sup>
+                    </div>
+                    <div className="des-wind">
+                        <p>{weather.data.weather[0].description.toUpperCase()}</p>
+                        <p>Wind Speed: {weather.data.wind.speed}m/s</p>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default WeatherApp;
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import WeatherApp from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <WeatherApp />
+)
